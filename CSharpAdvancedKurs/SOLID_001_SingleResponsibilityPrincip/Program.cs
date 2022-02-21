e65bf1b7130724a049e1c073a747bda15cbe691d
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-02-21 15:17:02 +0100

    SOLID

---
+++ CSharpAdvancedKurs/SOLID_001_SingleResponsibilityPrincip/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+#region Anti-Beispiel
+
+
+public class EmployeeBad
+{
+    //Datenstruktur einer Tabelle in einer DB -> 
+    public int Id { get; set; } //Properties
+    public string Name { get; set; }    
+
+
+    //InsertEmployeeToTable -> DataAccess Layer -> (MEthode redet mit DB) 
+    public void InsertEmployeeToTable(EmployeeBad employee)
+    {
+        //any Code
+    }
+
+    //Ausgabe-> UI Präsentation Layer oder Service Layer (als JSON) 
+    public void GenerateReport()
+    {
+        //any Code
+    }
+}
+#endregion
+
+#region Besser Variante
+
+public class Employee
+{
+    public int Id { get; set; } 
+
+    public string Name { get; set; }    
+}
+
+//Ein Repository stellt das Manipilieren und Auslesesn gegenüber einer DB->Tabelle dar. 
+//Eine REpository-Klasse beartbeitet eine Tabelle -> 1:1 
+public class EmployeeRepository
+{
+    public void Insert(Employee employee)
+    {
+        //Any Code für das Speichern eines Employee Datensatzes
+    }
+}
+
+public class EmployeeReport
+{
+    public void GenerateReport()
+    {
+
+    }
+}
+#endregion
---
+++ CSharpAdvancedKurs/SOLID_002_OpenClosePrinzip/Program.cs
@@ -0,0 +1,161 @@
+﻿
+using System.Collections.Generic;
+
+CrystalReportGenerator crg1 = new CrystalReportGenerator();
+
+FastReportGenerator fast1 = new FastReportGenerator();
+
+ABCReporterGenerator abc1 = new ABCReporterGenerator();
+
+List<EmployeeGenerator> liste = new List<EmployeeGenerator>();
+liste.Add(crg1);
+liste.Add(fast1);
+liste.Add(abc1);    
+
+foreach(EmployeeGenerator e in liste)
+{
+    //Employee
+    e.GeneratoreReport(new Employee() { Id=1, Name="Peter Musterfrau"});
+}
+
+
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
+
+
+#region Bad Code
+public class BadReportGenerator
+{
+    public string ReportType { get; set; } = string.Empty;
+
+
+    public string[] GetAllTemplates ()
+    {
+        if (ReportType == "CR")
+        {
+            //Generiere Crstal Reports
+            //
+
+            ///
+
+            // CR
+
+            // CR
+        }
+        else if (ReportType == "FastReport")
+        {
+            //Dll wird hier angeteuert
+            //Generiere Fast Reports 
+
+
+            //FastReport hier und da
+        }
+        else
+        {
+            //Generiere PDF 
+
+            //PDF 
+        }
+
+
+        return Array.Empty<string>();
+    }
+    public void GnerateReport(Employee em)
+    {
+        //Gibt es hier komische Initialisierungen
+        if (ReportType == "CR")
+        {
+            //Generiere Crstal Reports
+        }
+        else if(ReportType == "FastReport")
+        {
+            //Dll wird hier angeteuert
+            //Generiere Fast Reports 
+
+
+            //Trifft die Initialisierung auf mich hier zu? 
+        }
+        else  
+        {
+            //Generiere PDF 
+        }
+    }
+}
+#endregion
+
+#region Good Code
+
+#region Open-Part mit abstrakte Klasse und interface
+//abstraktion -> Open-Part 
+public abstract class EmployeeGenerator
+{
+
+    //abstrakten Methoden MÜSSEN in der Vererbten Klasse überschrieben werden 
+    public abstract void GeneratoreReport(Employee em);
+}
+
+//Alternative  
+public interface IEmployeeGenerator
+{ 
+    //nterface muss beim Implementiert werden
+    void GeneratoreReport(Employee em);
+}
+
+
+public class CrystalReportGenerator : EmployeeGenerator
+{
+    public override void GeneratoreReport(Employee em)
+    {
+       //Crystal Report wird generiert 
+    }
+
+    //Weitere MEthoden die hier hinzukommen, arbeiten nur mit CR-Library.dll zusammena
+}
+
+public class FastReportGenerator : EmployeeGenerator
+{
+    public override void GeneratoreReport(Employee em)
+    {
+        //Fast Report
+    }
+
+    //Weitere Methoden hinzufügen, die FastReport relevant sind 
+}
+
+
+public class ABCReporterGenerator : EmployeeGenerator
+{
+    public override void GeneratoreReport(Employee em)
+    {
+        //Fast Report
+    }
+
+    //Weitere Methoden hinzufügen, die FastReport relevant sind 
+}
+
+
+#endregion
+
+
+
+
+#region abstrakt vs Interface
+public abstract class ReportGeneratorBase<T> 
+{
+    public abstract void GenerateItem(T itemToGenerate);
+
+    public void Konfigurations()
+    {
+        //Basis-Implementierung -> Alle abgeleitete Klassen können diese Methode aufrufen
+    }
+}
+
+
+
+#endregion
+#endregion
